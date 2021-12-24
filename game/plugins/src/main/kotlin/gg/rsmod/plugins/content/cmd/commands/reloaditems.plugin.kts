26dd8e9133c95749036f46b4f4bce89124b4c73d
CloudS3c <nerux2014@gmail.com>
2021-12-24 14:05:38 +0200

    Removed

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/cmd/commands/reloaditems.plugin.kts
+++
@@ -1,9 +0,0 @@
-import gg.rsmod.game.service.game.ItemMetadataService
-
-/**
- * @TODO Finish
- */
-on_command("reloaditems") {
-    load_service(ItemMetadataService())
-    player.message("ItemMetaDataService was reloaded.")
-}
