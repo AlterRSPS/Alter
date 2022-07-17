5520591cdb5e29be7111c1a22399fe76f62ece8d
CloudS3c <nerux2014@gmail.com>
2022-07-17 02:15:54 +0200

    Tournament Supplies rewrite to safer varient.

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/inter/tournament_supplies/Tournament_Supplies.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/inter/tournament_supplies/Tournament_Supplies.plugin.kts
@@ -1,18 +1,15 @@
 package gg.rsmod.plugins.content.inter.tournament_supplies
 
 import gg.rsmod.game.model.attr.INTERACTING_ITEM_ID
+import gg.rsmod.game.model.attr.INTERACTING_SLOT_ATTR
 import gg.rsmod.game.model.priv.Privilege
+import java.lang.NullPointerException
 
-var TOURNAMENT_SUPPLIES_INTERFACE = 100
-
-on_interface_open(interfaceId = TOURNAMENT_SUPPLIES_INTERFACE) p@ {
-    if (player.privilege.powers.contains(Privilege.DEV_POWER)) {
-        player.setInterfaceEvents(TOURNAMENT_SUPPLIES_INTERFACE, component = 4, range = 0..443, setting = 1086)
-    } else {
-        World.logger.warn("${player.username} was able to open Tournament Supplies Interface.")
-    }
+on_command("tournament", Privilege.ADMIN_POWER) {
+    Tournament_Supplies.open(player)
 }
-on_button(interfaceId = TOURNAMENT_SUPPLIES_INTERFACE, component = 4) {
+
+on_button(interfaceId = Tournament_Supplies.TOURNAMENT_SUPPLIES_INTERFACE, component = 4) {
     val itemid = player.attr[INTERACTING_ITEM_ID]!!
     val option = player.getInteractingOption()
     if (world.definitions.getCount(ItemDef::class.java) < itemid) {
@@ -41,3 +38,15 @@ on_button(interfaceId = TOURNAMENT_SUPPLIES_INTERFACE, component = 4) {
     }
 }
 
+on_button(interfaceId = Tournament_Supplies.TOURNAMENT_SUPPLIES_INVENTORY_INTERFACE, component = 0) {
+    try {
+        val slot = player.attr[INTERACTING_SLOT_ATTR]!!
+        val amount = when(player.getInteractingOption()) {
+            2 -> player.inventory.getItemCount(player.inventory[slot]!!.id)
+            else -> 1
+        }
+        player.inventory.remove(item = player.inventory[slot]!!.id, amount = amount , beginSlot = slot)
+    } catch (_: NullPointerException) {
+        // @TODO Ye dunno but some times when spamming to fast the click the ['Option'] goes null and console starts bitching
+    }
+}
