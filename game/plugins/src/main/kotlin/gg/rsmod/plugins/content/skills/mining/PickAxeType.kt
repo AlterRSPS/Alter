f26475bd26809a4c2da2a168a720d3a84f8a1a67
FrikkiFritz <Frikkipal@gmail.com>
2022-07-19 18:36:51 +0000

    Moved animations for mining and woodcutting into animation.kt

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/mining/PickAxeType.kt
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/mining/PickAxeType.kt
@@ -1,5 +1,6 @@
 package gg.rsmod.plugins.content.skills.mining
 
+import gg.rsmod.plugins.api.cfg.Animation
 import gg.rsmod.plugins.api.cfg.Items
 
 
@@ -7,17 +8,17 @@ import gg.rsmod.plugins.api.cfg.Items
  * @author Fritz <frikkipafi@gmail.com>
  */
 enum class PickAxeType(val item: Int, val level: Int, val animation: Int) {
-    BRONZE(Items.BRONZE_PICKAXE, level = 1, animation = 625),
-    IRON( Items.IRON_PICKAXE, level = 1, animation = 626),
-    STEEL(Items.STEEL_PICKAXE, level = 6, animation = 627),
-    BLACK(Items.BLACK_PICKAXE, level = 11, animation = 3873),
-    MITHRIL(Items.MITHRIL_PICKAXE, level = 21, animation = 629),
-    ADAMANT(Items.ADAMANT_PICKAXE, level = 31, animation = 628),
-    RUNE(Items.RUNE_PICKAXE, level = 41, animation = 624),
-    DRAGON(Items.DRAGON_PICKAXE, level = 61, animation = 7139),
-    INFERNAL(Items.INFERNAL_PICKAXE, level = 61, animation = 4482),
-    THIRDAGE(Items._3RD_AGE_PICKAXE, level = 61, animation = 7283),
-    CRYSTAL(Items.CRYSTAL_PICKAXE, level = 71, animation = 8347);
+    BRONZE(Items.BRONZE_PICKAXE, level = 1, Animation.BRONZE_MINE),
+    IRON(Items.IRON_PICKAXE, level = 1, Animation.IRON_MINE),
+    STEEL(Items.STEEL_PICKAXE, level = 6, Animation.STEEL_MINE),
+    BLACK(Items.BLACK_PICKAXE, level = 11, Animation.BLACK_MINE),
+    MITHRIL(Items.MITHRIL_PICKAXE, level = 21, Animation.MITHRIL_MINE),
+    ADAMANT(Items.ADAMANT_PICKAXE, level = 31, Animation.ADAMANT_MINE),
+    RUNE(Items.RUNE_PICKAXE, level = 41, Animation.RUNE_MINE),
+    DRAGON(Items.DRAGON_PICKAXE, level = 61, Animation.DRAGON_MINE),
+    INFERNAL(Items.INFERNAL_PICKAXE, level = 61, Animation.INFERNAL_MINE),
+    THIRDAGE(Items._3RD_AGE_PICKAXE, level = 61, Animation.THIRDAGE_MINE),
+    CRYSTAL(Items.CRYSTAL_PICKAXE, level = 71, Animation.CRYSTAL_MINE);
 
     companion object {
         val values = enumValues<PickAxeType>()
