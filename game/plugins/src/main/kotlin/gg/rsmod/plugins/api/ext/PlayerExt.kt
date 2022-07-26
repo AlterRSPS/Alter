5c978ebbde75a36ad00584bd187a782a01029104
FrikkiFritz <Frikkipal@gmail.com>
2022-07-26 23:27:32 +0000

    Url Opening fixed

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/api/ext/PlayerExt.kt
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/api/ext/PlayerExt.kt
@@ -25,6 +25,8 @@ import gg.rsmod.plugins.api.cfg.Varps
 import gg.rsmod.plugins.content.music.Songs
 import gg.rsmod.plugins.service.marketvalue.ItemMarketValueService
 import gg.rsmod.util.BitManipulation
+import java.awt.Desktop
+import java.net.URI
 
 /**
  * The interface key used by inventory overlays
@@ -66,9 +68,19 @@ fun Player.filterableMessage(message: String) {
     write(MessageGameMessage(type = ChatMessageType.SPAM.id, message = message, username = null))
 }
 
+fun Player.openUrl(url: String){
+    try {
+        Desktop.getDesktop().browse(URI(url))
+    } catch (e:Exception) {
+        e.printStackTrace()
+    }
+}
+
+/**
 fun Player.openUrl(url: String) {
     write(OpenUrlMessage(url))
 }
+*/
 
 fun Player.runClientScript(id: Int, vararg args: Any) {
     write(RunClientScriptMessage(id, *args))
