c85daf59450cd969e2a5963ae72912246303d730
CloudS3c <nerux2014@gmail.com>
2022-08-13 10:41:12 +0200

    This is bit weird.,

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/area/varrock/chat/aubury.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/area/varrock/chat/aubury.plugin.kts
@@ -2,23 +2,29 @@ package gg.rsmod.plugins.content.area.varrock.chat
 
 spawn_npc(Npcs.AUBURY_11435, 3253, 3402, 0, 2)
 
-arrayOf(Npcs.AUBURY_11435).forEach { aubury ->
-    on_npc_option(npc = aubury, option = "trade", lineOfSightDistance = 1) { open_shop(player) }
+arrayOf(Npcs.AUBURY_11434, Npcs.AUBURY_11435, Npcs.AUBURY).forEach { aubury ->
 
-    on_npc_option(npc = aubury, option = "talk-to", lineOfSightDistance = 1) { player.queue { dialog() } }
+    if (if_npc_has_option(npc = aubury, option = "trade")) {
+        on_npc_option(npc = aubury, option = "trade") { open_shop(player) }
+    }
 
-    on_npc_option(npc = aubury, option = "teleport", lineOfSightDistance = 1) {
+    if (if_npc_has_option(npc = aubury, option = "talk-to")) {
+        on_npc_option(npc = aubury, option = "talk-to", lineOfSightDistance = 1) { player.queue { dialog() } }
+    }
 
-        player.queue {
-            val npc = player.getInteractingNpc()
-            player.lock = LockState.FULL
-            npc.forceChat("Senventior Disthine Molenko")
-            npc.graphic(108, 10)
-            wait(3)
-            player.graphic(110, 125)
-            wait(2)
-            player.moveTo(2912, 4833, 0)
-            player.lock = LockState.NONE
+    if (if_npc_has_option(npc = aubury, option = "teleport")) {
+        on_npc_option(npc = aubury, option = "teleport", lineOfSightDistance = 1) {
+            player.queue {
+                val npc = player.getInteractingNpc()
+                player.lock = LockState.FULL
+                npc.forceChat("Senventior Disthine Molenko")
+                npc.graphic(108, 10)
+                wait(3)
+                player.graphic(110, 125)
+                wait(2)
+                player.moveTo(2912, 4833, 0)
+                player.lock = LockState.NONE
+            }
         }
     }
 }
