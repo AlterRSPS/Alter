174a603e02bded4be28b106d109c75885ebea8fb
koroiya <mooneykohner@gmail.com>
2022-08-16 22:58:23 -0500

    Added a method to right-click "climb up/climb down. A bit buggy.

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/objs/ladder/ladder.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/objs/ladder/ladder.plugin.kts
@@ -1,28 +1,35 @@
 package gg.rsmod.plugins.content.objs.ladder
-
+/**Climbing up ladders*/
 arrayOf(Objs.LADDER_12964, Objs.LADDER_12965, Objs.LADDER_16683).forEach { ladder_up ->
     on_obj_option(obj = ladder_up, option = "climb-up") {
         climbupladder(player)
     }
 }
 
+/**Interacting with multilevel ladders.*/
 arrayOf(Objs.LADDER_12965, Objs.LADDER_12966, Objs.LADDER_16679).forEach { ladder_down ->
     on_obj_option(obj = ladder_down, option = "climb-down") {
         climbdownladder(player)
     }
 }
 
+/**Climbing down stairs*/
 arrayOf(Objs.LADDER_12965, Objs.LADDER_16684).forEach { ladder ->
     on_obj_option(obj = ladder, option = "climb") {
         player.queue {
-            when (options("Climb Up", "Climb Down")) {
+            when (options("Climb up the ladder.", "Climb down the ladder.")) {
                 1 -> climbupladder(player)
                 2 -> climbdownladder(player)
+            }
+            on_obj_option(obj = ladder, option = "climb-up") {
+                climbupladder(player)
+            }
+            on_obj_option(obj = ladder, option = "climb-down") {
+                climbdownladder(player)
             }
         }
     }
 }
-
 
 fun climbupladder(player : Player) {
     player.queue {
@@ -43,3 +50,5 @@ fun climbdownladder(player : Player) {
         player.unlock()
     }
 }
+
+/**@TODO - Fix right-click to move.*/
