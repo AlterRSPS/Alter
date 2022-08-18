cbd8c399862a3b23a34e0ca4d00d3922ee45d541
koroiya <mooneykohner@gmail.com>
2022-08-18 06:44:43 -0500

    Fixed ladder in Lumbridge basement.

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/objs/ladder/ladder.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/objs/ladder/ladder.plugin.kts
@@ -35,7 +35,6 @@ val ladders = arrayOf(
     Objs.LADDER_12966,
     Objs.LADDER_16679,
     Objs.LADDER_16684,
-    Objs.LADDER_17385
 )
 
 ladders.forEach { ladder ->
@@ -106,8 +105,11 @@ fun climbstairs(player: Player) {
     }
 }
 
-/**Trapdoors*/
+/**Trapdoors.*/
 
 on_obj_option(Objs.TRAPDOOR_14880, option = "climb-down") {
     player.moveTo(3210, 9616, 0)
+}
+on_obj_option(Objs.LADDER_17385, option = "climb-up") {
+    player.moveTo(3210, 3216, 0)
 }
