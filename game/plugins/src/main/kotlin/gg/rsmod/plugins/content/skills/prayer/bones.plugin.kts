c4f56f8c0553e4e35dd6d98c40a59033d0eca435
Fritzl <frikkipal@gmail.com>
2022-08-19 21:40:13 +0000

    Offering to altar enabled and added saving bones chance to chaos altar

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/prayer/bones.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/prayer/bones.plugin.kts
@@ -13,17 +13,14 @@ Bones.values.forEach { bones ->
 
         }
     }
-    /**not working.
     on_item_on_obj(obj = Objs.CHAOS_ALTAR_411, item = bones.id) {
-        player.message("You offer the to Chaos altar")
         if (!Offer.canOffer(player, bones)) {
-            return@item_on_obj
+            return@on_item_on_obj
         }
-        val inventorySlot = player.getInteractingItemSlot()
-        if (player.inventory.remove(item = bones.id, beginSlot = inventorySlot).hasSucceeded()) {
+        if (player.inventory.contains(bones.id)) {
             // 1(gilded), 2(ecto), 3(chaos)
             Offer.OfferBones(player, bones, 3)
 
         }
-    }**/
+    }
 }
