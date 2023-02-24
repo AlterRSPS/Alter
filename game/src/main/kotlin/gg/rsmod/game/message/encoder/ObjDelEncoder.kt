6d59ee264bd40b3c86263e220505402a677e6d85
CloudS3c <nerux2014@gmail.com>
2023-02-24 21:54:37 +0100

    OBJ_del from legacy => New

--- game/src/main/kotlin/gg/rsmod/game/message/encoder/ObjDelEncoder.kt
+++ game/src/main/kotlin/gg/rsmod/game/message/encoder/ObjDelEncoder.kt
@@ -11,7 +11,7 @@ class ObjDelEncoder : MessageEncoder<ObjDelMessage>() {
     override fun extract(message: ObjDelMessage, key: String): Number = when (key) {
         "item" -> message.item
         "tile" -> message.tile
-        "unknown" -> 31
+        "amount" -> message.amount
         else -> throw Exception("Unhandled value key.")
     }
 
--- game/src/main/kotlin/gg/rsmod/game/model/region/update/EntityUpdateType.kt
+++ game/src/main/kotlin/gg/rsmod/game/model/region/update/EntityUpdateType.kt
@@ -10,13 +10,13 @@ enum class EntityUpdateType(val id: Int) {
     MAP_ANIM(id = 0), // MAP_ANIM
     UNKNOWN(id = 1), // PREFETCH_GAMEOBJECTS
     SPAWN_PROJECTILE(id = 2), //  MAPPROJ_ANIM
-    OBJ_DEL_NEW(id = 3),
+    REMOVE_GROUND_ITEM(id = 3),
     UPDATE_GROUND_ITEM(id = 4), // OBJ_COUNT
     ANIMATE_OBJECT(id = 5), // LOC_ANIM
     SPAWN_OBJECT(id = 6), // LOC_ADD_CHANGE
     REMOVE_OBJECT(id = 7), // LOC_DEL
-    MAP_ANIM_NEW(id = 8),
-    REMOVE_GROUND_ITEM(id = 9), // OBJ_DEL
+    MAP_ANIM_NEW(id = 8), // MAP_ANIM
+    LEGACY_GROUND_ITEM(id = 9), // OBJ_DEL
     SPAWN_GROUND_ITEM(id = 10), // OBJ_ADD
     PLAY_TILE_SOUND(id = 11), // AREA_SOUND
     OBJ_COUNT_2(id = 12)
