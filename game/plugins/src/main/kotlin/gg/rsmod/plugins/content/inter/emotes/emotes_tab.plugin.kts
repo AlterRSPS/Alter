f7bfe80a2a9e5f5644959c498624df699c85387e
CloudS3c <nerux2014@gmail.com>
2023-03-12 14:23:52 +0100

    rev212 known problems: Public Chat. And haven't added yet the friends packets and etc. Since they aren't implemented correctly.

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/inter/emotes/emotes_tab.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/inter/emotes/emotes_tab.plugin.kts
@@ -4,7 +4,7 @@ import gg.rsmod.plugins.content.inter.emotes.EmotesTab.COMPONENT_ID
 import gg.rsmod.plugins.content.inter.emotes.EmotesTab.performEmote
 
 on_login {
-    player.setInterfaceEvents(interfaceId = COMPONENT_ID, component = 2, range = 0..51, setting = arrayOf(InterfaceEvent.BUTTON1, InterfaceEvent.BUTTON2))
+    player.setInterfaceEvents(interfaceId = COMPONENT_ID, component = 2, range = 0..51, setting = arrayOf(InterfaceEvent.ClickOp1, InterfaceEvent.ClickOp2))
 }
 
 on_button(interfaceId = COMPONENT_ID, component = 2) p@ {
--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/firemaking/firemaking.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/firemaking/firemaking.plugin.kts
@@ -4,4 +4,8 @@ LogData.values().forEach { log ->
     on_item_on_item(Items.TINDERBOX, log.log) {
         player.queue { Firemaking.burnLog(this, log) }
     }
+    on_ground_item_option(log.log, "Light") {
+        player.graphic(555);
+        player.message("Seems to be working?")
+    }
 }
