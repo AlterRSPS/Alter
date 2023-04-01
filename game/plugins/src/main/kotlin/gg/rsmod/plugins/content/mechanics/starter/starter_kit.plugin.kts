f1440b9e4744a7c8d9e0d9da05c72ce214c2ccc9
CloudS3c <nerux2014@gmail.com>
2023-04-01 18:33:05 +0200

    Rewrote starting kit for new players will be improved in feature just not priority now.

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/mechanics/starter/starter_kit.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/mechanics/starter/starter_kit.plugin.kts
@@ -3,43 +3,14 @@ package gg.rsmod.plugins.content.mechanics.starter
 import gg.rsmod.game.model.attr.NEW_ACCOUNT_ATTR
 
 on_login {
-    val newAccount = player.attr[NEW_ACCOUNT_ATTR] ?: return@on_login
-    if (newAccount) {
-        val inventory = player.getInventoryStarterItems()
-        val bank = player.getBankStarterItems()
-
-        inventory.forEach { slotItem ->
-            player.inventory.add(item = slotItem.item, beginSlot = slotItem.slot)
-        }
-
-        bank.forEach { slotItem ->
-            player.bank.add(item = slotItem.item, beginSlot = slotItem.slot)
+    if (player.attr[NEW_ACCOUNT_ATTR] ?: return@on_login) {
+        with (player.inventory) {
+            add(Items.LOGS, 5)
+            add(Items.TINDERBOX)
+            add(Items.BREAD, 5)
+            add(Items.BRONZE_PICKAXE)
+            add(Items.BRONZE_DAGGER)
+            add(Items.KNIFE)
         }
     }
 }
-
-fun Player.getInventoryStarterItems() = getStarterItems(inventory.capacity, { getItem }, { getItemAmount })
-
-fun Player.getBankStarterItems() = getStarterItems(bank.capacity, { getBankItem }, { getBankItemAmount })
-
-fun getStarterItems(containerCapacity: Int, itemProperty: (Int).() -> String, amountProperty: (Int).() -> String): List<SlotItem> {
-    val items = mutableListOf<SlotItem>()
-    for (i in 0 until containerCapacity) {
-        val item = getProperty<Int>(itemProperty(i)) ?: continue
-        val amt = getProperty<Int>(amountProperty(i)) ?: 1
-        items.add(SlotItem(i, Item(item, amt)))
-    }
-    return items
-}
-
-val Int.getItem: String
-    get() = "item[$this]"
-
-val Int.getItemAmount: String
-    get() = "amount[$this]"
-
-val Int.getBankItem: String
-    get() = "bank_item[$this]"
-
-val Int.getBankItemAmount: String
-    get() = "bank_amount[$this]"
