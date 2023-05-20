e39d327396ad84cc8cd51f8dfee60e30f35fbda1
CloudS3c <nerux2014@gmail.com>
2023-05-20 03:47:45 +0200

    Tournament Supplies Interface fixed.

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/interfaces/tournament_supplies/Tournament_Supplies.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/interfaces/tournament_supplies/Tournament_Supplies.plugin.kts
@@ -28,24 +28,35 @@ on_button(interfaceId = Tournament_Supplies.TOURNAMENT_SUPPLIES_INTERFACE, compo
     if (world.definitions.getCount(ItemDef::class.java) < itemid) {
         player.message("[Unhandled item] - $itemid")
     } else {
-        var amount = when(option) {
-            1 -> 1
-            2 -> 5
-            3 -> 10
-            4 -> -1
+        if (option == 9) {
+            world.sendExamine(player, itemid, type = ExamineEntityType.ITEM)
+            return@on_button
+        }
+        var amount = when (option) {
+            0 -> 1
+            1 -> 5
+            2 -> 10
+            3 -> -1
             else -> return@on_button
         }
         if (amount == -1) {
             player.queue(TaskPriority.WEAK) {
                 amount = inputInt("How many would you like to withdraw?")
                 if (amount > 0) {
-                    if (player.inventory.freeSlotCount < amount && !world.definitions.get(ItemDef::class.java, itemid).stackable) {
+                    if (player.inventory.freeSlotCount < amount && !world.definitions.get(
+                            ItemDef::class.java,
+                            itemid
+                        ).stackable
+                    ) {
                         amount = player.inventory.freeSlotCount
                     }
                     player.inventory.add(itemid, amount)
                 }
             }
         } else {
+            if (world.definitions.get(ItemDef::class.java, itemid).stackable) {
+                amount = 10000
+            }
             player.inventory.add(itemid, amount)
         }
     }
@@ -53,10 +64,18 @@ on_button(interfaceId = Tournament_Supplies.TOURNAMENT_SUPPLIES_INTERFACE, compo
 
 on_button(interfaceId = Tournament_Supplies.TOURNAMENT_SUPPLIES_INVENTORY_INTERFACE, component = 0) {
     try {
+        val opt = player.getInteractingOption()
         val slot = player.attr[INTERACTING_SLOT_ATTR]!!
-        val amount = when(player.getInteractingOption()) {
-            2 -> player.inventory.getItemCount(player.inventory[slot]!!.id)
+        if (opt == 9) {
+            world.sendExamine(player, player.inventory[slot]!!.id, type = ExamineEntityType.ITEM)
+            return@on_button
+        }
+        var amount = when(opt) {
+            1 -> player.inventory.getItemCount(player.inventory[slot]!!.id)
             else -> 1
+        }
+        if (world.definitions.get(ItemDef::class.java, player.inventory[slot]!!.id).stackable) {
+            amount = player.inventory.getItemCount(player.inventory[slot]!!.id)
         }
         player.inventory.remove(item = player.inventory[slot]!!.id, amount = amount , beginSlot = slot)
     } catch (_: NullPointerException) {
