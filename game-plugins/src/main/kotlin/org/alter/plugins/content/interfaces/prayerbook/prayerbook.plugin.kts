0bc208fa71b3a272c726ba2e2fd3ecc2cf695a47
CloudS3c <nerux2014@gmail.com>
2023-06-18 17:37:55 +0200

    Venator Bow / Template for Prayer Book

---
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/interfaces/prayerbook/prayerbook.plugin.kts
@@ -0,0 +1,15 @@
+package org.alter.plugins.content.interfaces.prayerbook;
+
+/**
+ *  @author <a href="https://github.com/CloudS3c">Cl0ud</a>
+ *  @author <a href="https://www.rune-server.ee/members/376238-cloudsec/">Cl0ud</a>
+ *
+ */
+
+on_command("prayerbook") {
+    // player.setVarbit(14826) // 0 = For normal prayer book , 1 = for the new book
+    // ClientScript(id = 2158)
+    // ClientScript(id = 915, converted = [5], raw = [5], types = [i])
+    //IfCloseSub(topInterface = 164, topComponent = 16)
+    // IfSetEvents(interfaceId = 541, componentId = 41, startIndex = 0, endIndex = 4, events = ClickOp1)
+}
