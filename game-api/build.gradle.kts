2a75cdfe0920a37d11e14486f894f45f3848e78e
CloudS3c <nerux2014@gmail.com>
2023-06-18 12:43:50 +0200

    Dokka Plugin for Items/Npcs/and etc.. Suppression

--- game-api/build.gradle.kts
+++ game-api/build.gradle.kts
@@ -1,4 +1,19 @@
+import org.jetbrains.dokka.gradle.DokkaTask
+
+plugins {
+    id("org.jetbrains.dokka") version "0.9.18"
+}
+
 dependencies {
     implementation(project(":game-server"))
     implementation(project(":util"))
+}
+
+tasks.withType<DokkaTask> {
+    for (pkg in listOf("gg.rsmod.plugins.api.cfg")) {
+        packageOptions {
+            prefix = pkg
+            suppress = true
+        }
+    }
 }
