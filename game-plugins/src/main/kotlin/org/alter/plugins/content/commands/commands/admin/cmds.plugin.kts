41a76b5f7284136d7776657f0680939b9afc4d9e
CloudS3c <nerux2014@gmail.com>
2023-06-24 02:49:13 +0200

    Relocating command list methods will refactor later.

--- game-plugins/src/main/kotlin/org/alter/plugins/content/commands/commands/admin/cmds.plugin.kts
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/commands/commands/admin/cmds.plugin.kts
@@ -1,7 +1,25 @@
 import org.alter.game.model.priv.Privilege
+import org.alter.game.plugin.PluginRepository
 
 on_command(command = "cmds", powerRequired = Privilege.ADMIN_POWER, description = "Display commands") {
     player.setInterfaceUnderlay(color = -1, transparency = -1)
     player.openInterface(187, dest = InterfaceDestination.MAIN_SCREEN)
-    player.runClientScript(217, "Command List:", get_all_commands().joinToString("|").removePrefix("(").removePrefix(")"))
+    /**
+     * IfOpenSub(id = 187, topInterface = 164, topComponent = 16, modal = true)
+     * ClientScript(id = 217, converted = ["Debug", "Teleports|All Gear|Gear Loadouts|New Items|Swap Prayerbook|Swap Spellbook", true], raw = ["Debug", "Teleports|All Gear|Gear Loadouts|New Items|Swap Prayerbook|Swap Spellbook", 1], types = [ss1])
+     * IfSetEvents(interfaceId = 187, componentId = 3, startIndex = 0, endIndex = 127, events = Continue)
+     */
+    player.runClientScript(217, "Command List:", getCommands(getPluginRepository()).joinToString("|").removePrefix("(").removePrefix(")"))
+}
+
+fun getCommands(r: PluginRepository): List<String> {
+    val str_list = ArrayList<String>()
+    r.commandPlugins.forEach { (t, _) ->
+        var value = "::$t"
+        if (r.getDescription(t) != "") {
+            value += " = [ ${r.getDescription(t)} ]"
+        }
+        str_list.add(value)
+    }
+    return str_list
 }
