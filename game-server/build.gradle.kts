161cbf305e5837ce9d32bb7bf0709a6a99dd0057
CloudS3c <nerux2014@gmail.com>
2023-09-17 14:49:43 +0200

    From yaml to csv for entity examination message collection.

--- game-server/build.gradle.kts
+++ game-server/build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
         implementation(bouncycastle)
         implementation(jackson.module.kotlin)
         implementation(jackson.dataformat.yaml)
+        implementation(kotlin.csv)
         testImplementation(junit)
     }
 }
--- game-server/src/main/kotlin/org/alter/game/service/game/ObjectMetadataService.kt
+++ game-server/src/main/kotlin/org/alter/game/service/game/ObjectMetadataService.kt
@@ -1,17 +1,11 @@
 package org.alter.game.service.game
 
-import com.fasterxml.jackson.annotation.JsonInclude
-import com.fasterxml.jackson.core.type.TypeReference
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.PropertyNamingStrategy
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
 import org.alter.game.Server
 import org.alter.game.fs.DefinitionSet
 import org.alter.game.fs.def.ObjectDef
 import org.alter.game.model.World
 import org.alter.game.service.Service
 import gg.rsmod.util.ServerProperties
-import java.io.BufferedReader
 import java.io.FileNotFoundException
 import java.nio.file.Files
 import java.nio.file.Path
@@ -24,39 +18,34 @@ class ObjectMetadataService : Service {
 
     private lateinit var path: Path
 
-    override fun init(server: org.alter.game.Server, world: World, serviceProperties: ServerProperties) {
-        path = Paths.get(serviceProperties.getOrDefault("path", "../data/cfg/objs.yml"))
+    override fun init(server: Server, world: World, serviceProperties: ServerProperties) {
+        path = Paths.get(serviceProperties.getOrDefault("path", "../data/cfg/locs.csv"))
         if (!Files.exists(path)) {
             throw FileNotFoundException("Path does not exist. $path")
         }
-
-        Files.newBufferedReader(path).use { reader ->
-            load(world.definitions, reader)
-        }
+        load(world.definitions)
     }
 
-    override fun postLoad(server: org.alter.game.Server, world: World) {
+    override fun postLoad(server: Server, world: World) {
     }
 
-    override fun bindNet(server: org.alter.game.Server, world: World) {
+    override fun bindNet(server: Server, world: World) {
     }
 
-    override fun terminate(server: org.alter.game.Server, world: World) {
+    override fun terminate(server: Server, world: World) {
     }
 
-    private fun load(definitions: DefinitionSet, reader: BufferedReader) {
-        val mapper = ObjectMapper(YAMLFactory())
-        mapper.propertyNamingStrategy = PropertyNamingStrategy.CAMEL_CASE_TO_LOWER_CASE_WITH_UNDERSCORES
-        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL)
-
-        val reference = object : TypeReference<List<Metadata>>() {}
-        mapper.readValue<List<Metadata>>(reader, reference)?.let { metadataSet ->
-            metadataSet.forEach { metadata ->
-                val def = definitions.getNullable(ObjectDef::class.java, metadata.id) ?: return@forEach
-                def.examine = metadata.examine
+    private fun load(definitions: DefinitionSet) {
+        path.toFile().forEachLine { line ->
+            val parts = line.split(',')
+            if (parts.size >= 2) {
+                val id = parts[0].toIntOrNull()
+                val examine = line.substringAfter(',').trim()
+                if (id != null) {
+                    val def = definitions.getNullable(ObjectDef::class.java, id) ?: return@forEachLine
+                    def.examine = examine.replace("\"", "")
+                }
             }
         }
     }
-
-    private data class Metadata(val id: Int = -1, val examine: String? = null)
 }
