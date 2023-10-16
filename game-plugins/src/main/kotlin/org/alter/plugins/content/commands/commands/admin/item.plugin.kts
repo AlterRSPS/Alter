90bf9bea16a7dfce17872cb62bc30be2e1ce993b
CloudS3c <nerux2014@gmail.com>
2023-10-16 02:38:46 +0200

    Added Exception to open spawn Interface if theres any when doing ::item command.

--- game-plugins/src/main/kotlin/org/alter/plugins/content/commands/commands/admin/item.plugin.kts
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/commands/commands/admin/item.plugin.kts
@@ -5,14 +5,42 @@ import java.text.DecimalFormat
 on_command("item", Privilege.ADMIN_POWER, description = "Spawn items") {
     val args = player.getCommandArgs()
     tryWithUsage(player, args, "Invalid format! Example of proper command <col=801700>::item 4151 1</col> or <col=801700>::item 4151</col>") { values ->
-        val item = values[0].toInt()
-        val amount = if (values.size > 1) Math.min(Int.MAX_VALUE.toLong(), values[1].parseAmount()).toInt() else 1
-        if (item < world.definitions.getCount(ItemDef::class.java)) {
-            val def = world.definitions.get(ItemDef::class.java, Item(item).toUnnoted(world.definitions).id)
-            val result = player.inventory.add(item = item, amount = amount, assureFullInsertion = false)
-            player.message("You have spawned <col=801700>${DecimalFormat().format(result.completed)} x ${def.name}</col></col> ($item).")
-        } else {
-            player.message("Item $item does not exist in cache.")
+        try {
+            val item = values[0].toInt()
+            val amount = if (values.size > 1) Math.min(Int.MAX_VALUE.toLong(), values[1].parseAmount()).toInt() else 1
+            if (item < world.definitions.getCount(ItemDef::class.java)) {
+                val def = world.definitions.get(ItemDef::class.java, Item(item).toUnnoted(world.definitions).id)
+                val result = player.inventory.add(item = item, amount = amount, assureFullInsertion = false)
+                player.message("You have spawned <col=801700>${DecimalFormat().format(result.completed)} x ${def.name}</col></col> ($item).")
+            } else {
+                player.message("Item $item does not exist in cache.")
+            }
+        } catch (e: Exception) {
+            player.queue(TaskPriority.STRONG) {
+                val item = spawn() ?: return@queue
+                if (item.amount > 0) {
+                    player.message("You have spawned ${item.amount} x ${item.getName(world.definitions)}.")
+                } else {
+                    player.message("You don't have enough inventory space.")
+                }
+            }
         }
     }
+}
+
+
+suspend fun QueueTask.spawn(): Item? {
+    val item = searchItemInput("Select an item to spawn:")
+    if (item == -1) {
+        return null
+    }
+    val amount = when (options("1", "5", "X", "Max", title = "How many would you like to spawn?")) {
+        1 -> 1
+        2 -> 5
+        3 -> inputInt("Enter amount to spawn")
+        4 -> Int.MAX_VALUE
+        else -> return null
+    }
+    val add = player.inventory.add(item, amount, assureFullInsertion = false)
+    return Item(item, add.completed)
 }
