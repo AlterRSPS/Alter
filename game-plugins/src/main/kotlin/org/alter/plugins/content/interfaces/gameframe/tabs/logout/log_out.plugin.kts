665795a29537ab51cb228c3059e5998c2cc284b4
CloudS3c <nerux2014@gmail.com>
2024-07-06 21:00:12 +0200

    Logout patch

--- game-plugins/src/main/kotlin/org/alter/plugins/content/interfaces/gameframe/tabs/logout/log_out.plugin.kts
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/interfaces/gameframe/tabs/logout/log_out.plugin.kts
@@ -9,7 +9,7 @@ on_button(interfaceId = 182, component = 8) {
     if (!player.timers.has(ACTIVE_COMBAT_TIMER)) {
         player.requestLogout()
         player.write(net.rsprot.protocol.game.outgoing.logout.Logout)
-        player.channelClose()
+        player.channelFlush()
     } else {
         player.message("You can't log out until 10 seconds after the end of combat.")
     }
