65d7956571acd04701908ea335d403b2a6eca0b4
Gabriele Savickyte <nerux2014@gmail.com>
2024-07-15 12:56:22 +0200

    Enabled Ktlint, for refactoring

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,14 +3,14 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     alias(libs.plugins.kotlin.jvm) apply true
     alias(libs.plugins.kotlin.serialization)
-    //alias(libs.plugins.ktlint)
+    alias(libs.plugins.ktlint)
     idea
 }
 
 allprojects {
     apply(plugin = "idea")
     apply(plugin = "org.jetbrains.kotlin.jvm")
-    //apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
 
     group = "org.alter"
     version = "0.0.5"
