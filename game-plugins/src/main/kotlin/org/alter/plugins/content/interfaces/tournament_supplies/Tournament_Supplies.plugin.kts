c0d28a87c5513c7d923fb54dcdedff1a03645866
CloudS3c <nerux2014@gmail.com>
2024-07-27 14:16:10 +0200

    fix: Corrected option numbers and improved Tournament interface

--- game-plugins/src/main/kotlin/org/alter/plugins/content/interfaces/tournament_supplies/Tournament_Supplies.plugin.kts
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/interfaces/tournament_supplies/Tournament_Supplies.plugin.kts
@@ -1,61 +1,45 @@
 package org.alter.plugins.content.interfaces.tournament_supplies
 
-import dev.openrune.cache.CacheManager.getEnum
 import dev.openrune.cache.CacheManager.getItem
-import dev.openrune.cache.CacheManager.itemSize
+import org.alter.game.model.attr.INTERACTING_ITEM_ID
 import org.alter.game.model.attr.INTERACTING_SLOT_ATTR
 import org.alter.game.model.priv.Privilege
-
-val itemListBySlot = hashMapOf<Int, Int>()
-
-on_world_init {
-    val itemEnum = getEnum(1124)
-    itemEnum.values.forEach { it ->
-        val item = it.value as Int
-        val slot = it.key as Int
-        itemListBySlot[slot] = item
-    }
-}
 
 on_command("tournament", Privilege.ADMIN_POWER) {
     Tournament_Supplies.open(player)
 }
 
 on_button(interfaceId = Tournament_Supplies.TOURNAMENT_SUPPLIES_INTERFACE, component = 4) {
-    val itemid = itemListBySlot[player.attr[INTERACTING_SLOT_ATTR]!!]!!
+    val itemid = player.attr[INTERACTING_ITEM_ID]!!
     val option = player.getInteractingOption()
-    if (itemSize() < itemid) {
-        player.message("[Unhandled item] - $itemid")
-    } else {
-        if (option == 9) {
-            world.sendExamine(player, itemid, type = ExamineEntityType.ITEM)
-            return@on_button
+    if (option == 9) {
+        world.sendExamine(player, itemid, type = ExamineEntityType.ITEM)
+        return@on_button
+    }
+    var amount =
+        when (option) {
+            1 -> 1
+            2 -> 5
+            3 -> 10
+            4 -> -1
+            else -> return@on_button
         }
-        var amount =
-            when (option) {
-                0 -> 1
-                1 -> 5
-                2 -> 10
-                3 -> -1
-                else -> return@on_button
-            }
-        if (amount == -1) {
-            player.queue(TaskPriority.WEAK) {
-                amount = inputInt("How many would you like to withdraw?")
-                if (amount > 0) {
-                    if (player.inventory.freeSlotCount < amount && !getItem(itemid).stackable
-                    ) {
-                        amount = player.inventory.freeSlotCount
-                    }
-                    player.inventory.add(itemid, amount)
+    if (amount == -1) {
+        player.queue(TaskPriority.WEAK) {
+            amount = inputInt("How many would you like to withdraw?")
+            if (amount > 0) {
+                if (player.inventory.freeSlotCount < amount && !getItem(itemid).stackable
+                ) {
+                    amount = player.inventory.freeSlotCount
                 }
+                player.inventory.add(itemid, amount)
             }
-        } else {
-            if (getItem(itemid).stackable) {
-                amount = 10000
-            }
-            player.inventory.add(itemid, amount)
         }
+    } else {
+        if (getItem(itemid).stackable) {
+            amount = 10000
+        }
+        player.inventory.add(itemid, amount)
     }
 }
 
@@ -76,7 +60,7 @@ on_button(interfaceId = Tournament_Supplies.TOURNAMENT_SUPPLIES_INVENTORY_INTERF
             amount = player.inventory.getItemCount(player.inventory[slot]!!.id)
         }
         player.inventory.remove(item = player.inventory[slot]!!.id, amount = amount, beginSlot = slot)
-    } catch (_: NullPointerException) {
-        // @TODO Ye dunno but some times when spamming to fast the click the ['Option'] goes null and console starts bitching
+    } catch (e: NullPointerException) {
+        e.printStackTrace()
     }
 }
