989849f20079f45b3341b949b541f969014fd854
CloudS3c <nerux2014@gmail.com>
2024-08-11 21:53:31 +0200

    chore: Some random comments that aren't needed.

--- game-api/src/main/kotlin/org/alter/api/cfg/Varp.kt
+++ game-api/src/main/kotlin/org/alter/api/cfg/Varp.kt
@@ -1,12 +1,9 @@
 package org.alter.api.cfg
 
 object Varp {
-    // Audio
     const val AUDIO_SOUND_EFFECT_VOLUME = 169
     const val AUDIO_AREA_SOUND_VOLUME = 872
     const val AUDIO_MUSIC_VOLUME = 168
-
-    // Chat
     const val CHAT_PROFANITY_FILTER = 1074
     const val CHAT_SHOW_CHAT_EFFECTS = 171
     const val CHAT_SPLIT_FRIENDS_CHAT = 287
