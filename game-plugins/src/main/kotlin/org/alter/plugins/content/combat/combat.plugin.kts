b23b909d2f557edc66d61dccfa1d4c294645f0aa
CloudS3c <nerux2014@gmail.com>
2025-01-08 15:39:09 +0100

    feat: Combat pathing improvements.

--- game-plugins/src/main/kotlin/org/alter/plugins/content/combat/combat.plugin.kts
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/combat/combat.plugin.kts
@@ -8,6 +8,7 @@ import org.alter.game.model.move.MovementQueue.StepType
 import org.alter.plugins.content.combat.specialattack.SpecialAttacks
 import org.alter.plugins.content.combat.strategy.magic.CombatSpell
 import org.alter.plugins.content.interfaces.attack.AttackTab
+import java.util.*
 
 setCombatLogic {
     pawn.attr[COMBAT_TARGET_FOCUS_ATTR]?.get()?.let { target ->
@@ -15,6 +16,7 @@ setCombatLogic {
     }
     pawn.queue {
         while (true) {
+            // @TODO Npc can follow player up to 16 tiles from spawn point, some npc will have exceptional range so property for overwrite should be added.
             if (!cycle(this)) {
                 break
             }
@@ -27,9 +29,8 @@ onPlayerOption("Attack") {
     val target = pawn.attr[INTERACTING_PLAYER_ATTR]?.get() ?: return@onPlayerOption
     player.attack(target)
 }
-
 /**
- * @TODO Add ranged destination. As right now it will always walk next to target, and then clean up and polish this spaghetti mess.
+ * @TODO Bigger creatures seem to have bugged range + their path finding sucks due to conditions given.
  */
 suspend fun cycle(queue: QueueTask): Boolean {
     val pawn = queue.pawn
@@ -37,22 +38,16 @@ suspend fun cycle(queue: QueueTask): Boolean {
     val strategy = CombatConfigs.getCombatStrategy(pawn)
     val attackRange = strategy.getAttackRange(pawn)
     var pathLogic = 1
-
     if (target != pawn.attr[FACING_PAWN_ATTR]?.get()) {
         return false
     }
-
-    /**
-     * Players always use Smart pathfinder,
-     * Npc most of them use Dumb pathfinder and some use smart pathfinder.
-     */
     if (pawn.entityType.isNpc) {
         pathLogic = (pawn as Npc).pathLogic
     }
-    val reached = world.reachStrategy.reached(
+    var reached = world.reachStrategy.reached(
         flags = world.collision,
         level = pawn.tile.height,
-        srcX = pawn.tile.x,
+        srcX = pawn.tile.x ,
         srcZ = pawn.tile.z,
         destX = target.tile.x,
         destZ = target.tile.z,
@@ -64,8 +59,6 @@ suspend fun cycle(queue: QueueTask): Boolean {
     if (!reached) {
         when (pathLogic) {
             1 -> {
-                println("p1")
-
                 val route = world.smartPathFinder.findPath(
                     level = pawn.tile.height,
                     srcX = pawn.tile.x,
@@ -77,11 +70,9 @@ suspend fun cycle(queue: QueueTask): Boolean {
                     destHeight = target.getSize()
                 )
                 pawn.walkPath(route, StepType.NORMAL)
-
             }
-
             0 -> {
-                println("p0")
+                val path = LinkedList<Tile>()
                 val destination = world.dumbPathFinder.naiveDestination(
                     sourceX = pawn.tile.x,
                     sourceZ = pawn.tile.z,
@@ -92,67 +83,36 @@ suspend fun cycle(queue: QueueTask): Boolean {
                     targetWidth = target.getSize(),
                     targetHeight = target.getSize()
                 )
-                val direction = Direction.between(pawn.tile, Tile(destination.x, destination.z))
-                /**
-                 * Ran behind a pillar and it caused inf loop freezing entire server
-                 */
-                if (!pawn.world.canTraverse(
-                        source = pawn.tile,
-                        direction = direction,
-                        pawn = pawn,
-                        srcSize = pawn.getSize()
-                    )
-                ) {
-                    val targetsTile = target.tile
-                    while (!pawn.tile.isWithinRadius(
-                            targetsTile,
-                            target.getSize() + attackRange
-                        ) && !pawn.hasMoveDestination()
-                    ) {
-                        listOfNotNull(
-                            Tile(1, 1),
-                            Tile(1, 0),
-                            Tile(0, 1)
-                        ).forEachIndexed { index, it ->
-                            println("$index : ${pawn.tile.transform(it.x, it.z)}")
-
-                            /**
-                             * @TODO This shit uses smart pathing btw
-                             */
-                            pawn.walkTo(Tile(pawn.tile.transform(it.x, it.z)))
-                            if (!world.canTraverse(
-                                    source = pawn.tile,
-                                    direction = Direction.between(
-                                        pawn.tile,
-                                        Tile(destination.x, destination.z).transform(it.x, it.z)
-                                    ),
-                                    pawn = pawn,
-                                    srcSize = pawn.getSize()
-                                )
-                            ) {
-                                queue.wait(1)
-                            } else {
-                                if (pawn.entityType.isNpc) {
-                                    return true
-                                }
-
-                            }
+                val dx = destination.x - pawn.tile.x
+                val dz = destination.z - pawn.tile.z
+                // Try diagonal move (both x and z)
+                val diagonalMove = Tile(pawn.tile.x + dx.coerceIn(-1, 1), pawn.tile.z + dz.coerceIn(-1, 1))
+                if (!world.canTraverse(pawn.tile, Direction.between(pawn.tile, diagonalMove), pawn, pawn.getSize())) {
+                    // If diagonal blocked, try horizontal (east/west)
+                    val horizontalMove = Tile(pawn.tile.x + dx.coerceIn(-1, 1), pawn.tile.z)
+                    if (!world.canTraverse(pawn.tile, Direction.between(pawn.tile, horizontalMove), pawn, pawn.getSize())) {
+                        // If horizontal blocked, try vertical (north/south)
+                        val verticalMove = Tile(pawn.tile.x, pawn.tile.z + dz.coerceIn(-1, 1))
+                        if (world.canTraverse(pawn.tile, Direction.between(pawn.tile, verticalMove), pawn, pawn.getSize())) {
+                            path.add(verticalMove)
                         }
-                        /**
-                         * We return true for the Npc to keep targeting the player -> Need to test with 2 Pawn's if the npc will change target or no.
-                         * @TODO Same need to check how this logic would happen on OSRS
-                         */
-
+                    } else {
+                        path.add(horizontalMove)
                     }
-
-
                 } else {
-                    pawn.walkPath(destination)
+                    path.add(diagonalMove)
                 }
-
-
+                if (path.isEmpty()) {
+                    pawn.forceChat("Broke")
+                    return true
+                }
+                pawn.walkPath(path, stepType = StepType.NORMAL)
             }
         }
+    }
+    if (pawn.tile.getDistance(target.tile) <= attackRange + target.getSize()) {
+        reached = true
+        pawn.stopMovement()
     }
     while (pawn.hasMoveDestination() || !reached) {
         queue.wait(1)
@@ -161,7 +121,6 @@ suspend fun cycle(queue: QueueTask): Boolean {
         }
         return cycle(queue)
     }
-
     if (!Combat.canEngage(pawn, target)) {
         Combat.reset(pawn)
         pawn.resetFacePawn()
