c56e8cc390046739c808a781bb9da1435aee5366
unknown <jpopple1505@hotmail.co.uk>
2025-02-16 21:35:41 +0000

    Small refactor of banker plugin

--- game-plugins/src/main/kotlin/org/alter/plugins/content/npcs/banker/BankerPlugin.kt
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/npcs/banker/BankerPlugin.kt
@@ -1,30 +1,27 @@
 package org.alter.plugins.content.npcs.banker
 
-import org.alter.api.*
-import org.alter.api.cfg.*
-import org.alter.api.dsl.*
+import org.alter.api.InterfaceDestination
 import org.alter.api.ext.*
-import org.alter.game.*
-import org.alter.game.model.*
-import org.alter.game.model.attr.*
-import org.alter.game.model.container.*
-import org.alter.game.model.container.key.*
-import org.alter.game.model.entity.*
-import org.alter.game.model.item.*
-import org.alter.game.model.queue.*
-import org.alter.game.model.shop.*
-import org.alter.game.model.timer.*
-import org.alter.game.plugin.*
+import org.alter.game.Server
+import org.alter.game.model.World
+import org.alter.game.model.entity.Player
+import org.alter.game.model.queue.QueueTask
+import org.alter.game.plugin.KotlinPlugin
+import org.alter.game.plugin.PluginRepository
 import org.alter.plugins.content.interfaces.bank.openBank
 
 class BankerPlugin(
-    r: PluginRepository,
-    world: World,
-    server: Server
+    r: PluginRepository, world: World, server: Server
 ) : KotlinPlugin(r, world, server) {
 
+    private val bankers = listOf(
+        "npc.banker_1479",
+        "npc.banker_1480",
+        "npc.banker_2897",
+    )
+
     init {
-        arrayOf("npc.banker_1479", "npc.banker_1480").forEach { banker ->
+        bankers.forEach { banker ->
             onNpcOption(npc = banker, option = "talk-to", lineOfSightDistance = 2) {
                 player.queue {
                     dialog(this)
@@ -34,7 +31,7 @@ class BankerPlugin(
                 player.openBank()
             }
             onNpcOption(npc = banker, option = "collect", lineOfSightDistance = 2) {
-                open_collect(player)
+                openCollect(player)
             }
         }
     }
@@ -43,21 +40,20 @@ class BankerPlugin(
         it.chatNpc("Good day, how may I help you?")
         when (options(it)) {
             1 -> it.player.openBank()
-            2 -> open_pin(it.player)
-            3 -> open_collect(it.player)
-            4 -> what_is_this_place(it)
+            2 -> openPin(it.player)
+            3 -> openCollect(it.player)
+            4 -> whatIsThisPlace(it)
         }
     }
 
-    suspend fun options(it: QueueTask): Int =
-        it.options(
-            "I'd like to access my bank account, please.",
-            "I'd like to check my PIN settings.",
-            "I'd like to collect items.",
-            "What is this place?",
-        )
+    suspend fun options(it: QueueTask): Int = it.options(
+        "I'd like to access my bank account, please.",
+        "I'd like to check my PIN settings.",
+        "I'd like to collect items.",
+        "What is this place?",
+    )
 
-    suspend fun what_is_this_place(it: QueueTask) {
+    private suspend fun whatIsThisPlace(it: QueueTask) {
         it.chatNpc("This is a branch of the Bank of Gielinor. We have<br>branches in many towns.", animation = 568)
         it.chatPlayer("And what do you do?", animation = 554)
         it.chatNpc(
@@ -66,12 +62,12 @@ class BankerPlugin(
         )
     }
 
-    fun open_collect(p: Player) {
+    private fun openCollect(p: Player) {
         p.setInterfaceUnderlay(color = -1, transparency = -1)
         p.openInterface(interfaceId = 402, dest = InterfaceDestination.MAIN_SCREEN)
     }
 
-    fun open_pin(p: Player) {
+    private fun openPin(p: Player) {
         p.setInterfaceUnderlay(color = -1, transparency = -1)
         p.openInterface(interfaceId = 14, dest = InterfaceDestination.MAIN_SCREEN)
     }
