bc64185ca0daa1a7f57787ded49923e52e278d4f
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-10-17 17:46:06 +0200

    Add thieving test spawns and update teleport location
    Introduces a new SpawnPlugin for the thieving test area, spawning various NPCs and objects for testing. Also updates the thieving teleport command to move the player to the new spawn area.

--- game-plugins/src/main/kotlin/org/alter/plugins/content/commands/commands/all/TeleportsPlugin.kt
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/commands/commands/all/TeleportsPlugin.kt
@@ -40,7 +40,7 @@ class TeleportsPlugin(
             player.moveTo(Tile(x = 2461, z = 3443, height = 0))
         }
         onCommand("thieving", description = "Teleports you to the test thieving") {
-            player.moveTo(Tile(x = 2600, z = 4726, height = 0))
+            player.moveTo(Tile(x = 2591, z = 4731, height = 0))
         }
     }
 }
